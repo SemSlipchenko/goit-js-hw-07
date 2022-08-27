ffa48f5e25f1554a082efb3f91b2920fb3a3f8c9
SemSlipchenko <semslipchenko@gmail.com>
2022-08-27 20:53:53 +0300

    create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(items) {
+    return items.map(({preview, original, description}) => { 
+        return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}" target="_parent">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+                </div>`;
+    }).join('');
+};
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
